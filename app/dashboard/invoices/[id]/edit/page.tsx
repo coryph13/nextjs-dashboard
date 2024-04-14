c8f05a5c325eba20f6a0ba689856565d1c9215d5
Cvdeby Coryphaues <coryph13@gmail.com>
2024-04-14 17:02:59 +0500

    not-found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import EditInvoiceForm from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
@@ -9,6 +10,9 @@ export default async function Page({ params }: { params: { id: string } }) {
         fetchCustomers()
     ]);
 
+    if (!invoice) {
+        notFound();
+    }
     return (
         <main>
             <Breadcrumbs 
@@ -19,5 +23,5 @@ export default async function Page({ params }: { params: { id: string } }) {
             />
             <EditInvoiceForm invoice={invoice} customers={customers} />
         </main>
-    )
+    );
 }
